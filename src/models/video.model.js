b56e8cc407c648ac0c06e01f0a9ecd59a95e76e8
vedantpDev <patilvedant333.com>
2024-04-26 10:59:51 +0530

    mongooseAggregatePaginate, bcrypt & jwt, custom methods for the schema

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    published: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = new model("Video", videoSchema);
