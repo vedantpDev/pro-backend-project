719a8f557e46bc2dd134f52cb234242ead8c84bb
vedantpDev <patilvedant333.com>
2024-05-07 00:07:23 +0530

    created all routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentuser,
+  getUserChannleProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImg,
 } from "../controllers/user.contoller.js";
 import { upload } from "../middlerwares/multer.middleware.js";
 import { verifyJWT } from "../middlerwares/auth.middlerware.js";
@@ -20,11 +27,21 @@ router.route("/register").post(
   ]),
   registerUser
 );
-
 router.route("/login").post(loginUser);
 
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentuser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-img")
+  .patch(verifyJWT, upload.single("coverImg"), updateUserCoverImg);
+router.route("/c/:userName").get(verifyJWT, getUserChannleProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
